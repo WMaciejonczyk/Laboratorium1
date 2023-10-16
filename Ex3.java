06712eab9d9fa8b59b42d3c5ab9f647a73ed7f38
WMaciejonczyk <147508482+WMaciejonczyk@users.noreply.github.com>
2023-10-16 21:44:16 +0200

    Update Ex3.java
    Kontrola wyjątków - rozwiązywanie problemu parsowania za pomocą bloku try-catch. Przykładowo może wyłapać błąd związany z podaniem znaków spoza systemu szestnastkowego.

--- Ex3.java
+++ Ex3.java
@@ -13,14 +13,18 @@ public class Ex3 {
             throw new IllegalArgumentException("Podany kod powinien składać się z sześciu znaków.");
         }
 
-        String s1 = k.substring(0, 2);
-        String s2 = k.substring(2, 4);
-        String s3 = k.substring(4, 6);
+        try {
+            String s1 = k.substring(0, 2);
+            String s2 = k.substring(2, 4);
+            String s3 = k.substring(4, 6);
 
-        int r = Integer.parseInt(s1, 16);
-        int g = Integer.parseInt(s2, 16);
-        int b = Integer.parseInt(s3, 16);
-        System.out.printf("(" + r + "," + b + "," + g + ")");
+            int r = Integer.parseInt(s1, 16);
+            int g = Integer.parseInt(s2, 16);
+            int b = Integer.parseInt(s3, 16);
+            System.out.printf("(" + r + "," + b + "," + g + ")");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
