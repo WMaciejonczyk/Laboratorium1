4949f70db9999010819aea8cedbb27dfd8070295
WMaciejonczyk <147508482+WMaciejonczyk@users.noreply.github.com>
2023-10-16 19:37:26 +0200

    Update Ex1.java
    Dołączenie komentarza dokumentacyjnego.

--- Ex1.java
+++ Ex1.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
+/**
+ * Zadanie 1.
+ */
 public class Ex1 {
+    /**
+     * Program obliczający dystans i wysokość rzutu ukośnego
+     * @param args prędkość oraz kąt uderzenia
+     * @throws IllegalArgumentException gdy prędkość jest ujemna lub kąt rzutu znajduje się poza zakresem 0-90 stopni
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
