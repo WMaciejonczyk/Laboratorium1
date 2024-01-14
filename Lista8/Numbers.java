37b50998708515f7db900f45fe0ce99361a694e5
WMaciejonczyk <147508482+WMaciejonczyk@users.noreply.github.com>
2024-01-14 16:54:26 +0100

    Update Numbers.java - zadanie 2.

--- Lista8/Numbers.java
+++ Lista8/Numbers.java
@@ -1,14 +1,39 @@
 /**
- * Klasa implementująca interfejs Runnable do drukowania liczb od 1 do 10.
+ * Klasa implementująca interfejs Runnable do zwiększania zadanego licznika.
  */
 public class Numbers implements Runnable {
+    private Counter counter;
+
+    /**
+     * Konstruktor przyjmujący jeden parametr
+     * @param counter licznik
+     */
+    public Numbers(Counter counter) {
+        this.counter = counter;
+    }
     /**
      * Metoda odpowiedzialna za wykonanie wątku
      */
     @Override
     public void run() {
-        for (int i = 1; i < 11; i++) {
-            System.out.print(i + " ");
+        for (int i = 1; i < 100001; i++) {
+            getCounter().increment();
         }
+    }
+
+    /**
+     * Metoda zwracająca licznik
+     * @return licznik
+     */
+    public Counter getCounter() {
+        return counter;
+    }
+
+    /**
+     * Metoda ustawiająca nowy licznik
+     * @param counter nowy licznik
+     */
+    public void setCounter(Counter counter) {
+        this.counter = counter;
     }
 }
