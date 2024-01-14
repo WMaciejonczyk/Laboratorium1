a37bb746df7ec67339103d6f255877b10864a956
WMaciejonczyk <147508482+WMaciejonczyk@users.noreply.github.com>
2024-01-14 17:00:04 +0100

    Update Counter.java - dokumentacja

--- Lista8/Counter.java
+++ Lista8/Counter.java
@@ -1,9 +1,20 @@
+/**
+ * Klasa tworząca obiekt licznika.
+ */
 public class Counter {
     private long count = 0;
+
+    /**
+     * Zsynchronizowana metoda inkrementacji licznika
+     */
     public synchronized void increment() {
         count = count + 1;
     }
 
+    /**
+     * Metoda zwracająca licznik
+     * @return
+     */
     public long getCount() {
         return count;
     }
